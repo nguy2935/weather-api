a47c2b97150f65f9ff915f40957e5ab50e354188
unknown <Sophia Nguyen>
2021-12-08 00:33:35 -0600

    setting up the api and added jquery and javascript linked to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// weather api 
+var appID = 'b002b3316d73d825f3942e6f84e94112';
+// var cityKey = api.openweathermap.org/data/2.5/weather?q={city name}&appid=b002b3316d73d825f3942e6f84e94112;
 
-$(function(){
-    var array = [];
-    $('searchBtn').on('click', function(e){
-       e.preventDefault();
-       array.push($('search').val());
-       $('search').val('');
-       $('results').val(array);
+// var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityID;
+var cityID = '';
+var getCityWeather = function(parameter) {
+   cityID = $('.searchBar').val();
+   var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityID + "&appid=" + appID;
+
+
+   $.getJSON( url, function( data ) {
+
     });
- });
+   console.log(url);
+}
+
+$(document).on('click', '.searchBtn', function (event) {
+   getCityWeather();
+});
+
+// parameters 
+// city.name for City name
+// main.temp for temp
+// wind.speed for wind speed
+// main.humidity for humidity
+// 
+
+// UV indexed
+// 5-Day Forecast
+// Last Searched stuff
