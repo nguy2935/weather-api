97120517d4ebf6b0ef5d0ae8ffa7791e35e623dc
unknown <Sophia Nguyen>
2021-12-09 02:24:35 -0600

    added dates for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ var getCityWeather = function(parameter) {
    cityID = $('.searchBar').val();
    var url = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + cityID + "&appid=" + appID;
 
-
    $.getJSON( url, function( data ) {
       console.log(url);
     }).done(function(data) {
@@ -21,41 +20,38 @@ var getCityWeather = function(parameter) {
          "lon": data.coord.lon
      }            
 
-     var urlUI = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityTemp.lat}&lon=${cityTemp.lon}&appid=${appID}`;
+     var urlUI = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${cityTemp.lat}&lon=${cityTemp.lon}&appid=${appID}`;
      $.getJSON( urlUI, function(data) {
         console.log(data);
    
      }).done(function(data) {
       cityTemp['uv'] = data.current.uvi;
       console.log(cityTemp);
-      console.log(data.daily[0].humidity);
-      console.log(data.daily[1].humidity);
-      console.log(data.daily[2].humidity);
-      console.log(data.daily[3].humidity);
-      console.log(data.daily[4].humidity);
       $('.citySearch').html(cityTemp.name);
       $('.temp strong').html(cityTemp.temp + ' °F');
       $('.windspeed strong').html(cityTemp.windspeed + ' MPH');
       $('.humidity strong').html(cityTemp.humidity + '%');
       $('.uvi span').html(cityTemp.uv)
+
+      $('.fiveDisplay').each(function(i){
+         i++;
+         console.log(data.daily[i].humidity);
+         var forecastDate = new Date(data.daily[i].dt * 1000);
+         $(this).find('.date strong').html(forecastDate.toLocaleDateString("en-US"));
+         $(this).find('.temp strong').html(data.daily[i].temp.max + ' °F');
+         $(this).find('.windspeed strong').html(data.daily[i].wind_speed + ' MPH');
+         $(this).find('.humidity strong').html(data.daily[i].humidity + '%');
+      });
+
+      $('.currentWeather').show();
+      $('.weekForecast').show();
       console.log( "second success" );
 
      }) 
     });
 }
 
-
+// the click button event listener
 $(document).on('click', '.searchBtn', function (event) {
    getCityWeather();
 });
-
-// parameters 
-// city.name for City name
-// main.temp for temp
-// wind.speed for wind speed
-// main.humidity for humidity
-// 
-
-// UV indexed
-// 5-Day Forecast
-// Last Searched stuff
