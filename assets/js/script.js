f8bafbbe84d7b2ff8a0e27f294cc1c58990c31c1
unknown <Sophia Nguyen>
2021-12-11 15:57:41 -0600

    removed console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ var getCityWeather = function(cityName) {
    var url = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + cityName + "&appid=" + appID;
 
    $.getJSON( url, function( data ) {
-      console.log(url);
     }).done(function(data) {
       var cityTemp = {
          "name": data.name,
@@ -23,11 +22,9 @@ var getCityWeather = function(cityName) {
 
      var urlUI = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${cityTemp.lat}&lon=${cityTemp.lon}&appid=${appID}`;
      $.getJSON( urlUI, function(data) {
-        console.log(data);
    
      }).done(function(data) {
       cityTemp['uv'] = data.current.uvi;
-      console.log(cityTemp);
       $('.citySearch').html(cityTemp.name + ` (${cityTemp.dt.toLocaleDateString("en-US")})`);
       $('.temp strong').html(cityTemp.temp + ' °F');
       $('.windspeed strong').html(cityTemp.windspeed + ' MPH');
@@ -36,7 +33,6 @@ var getCityWeather = function(cityName) {
 
       $('.fiveDisplay').each(function(i){
          i++;
-         console.log(data.daily[i].humidity);
          var forecastDate = new Date(data.daily[i].dt * 1000);
          $(this).find('.date strong').html(forecastDate.toLocaleDateString("en-US"));
          $(this).find('.temp strong').html(data.daily[i].temp.max + ' °F');
@@ -44,6 +40,7 @@ var getCityWeather = function(cityName) {
          $(this).find('.humidity strong').html(data.daily[i].humidity + '%');
       });
 
+      // this displays the current weather and 5-day forecast
       $('.currentWeather').show();
       $('.weekForecast').show();
       // this checks to see if it duplicates stored in localstorage
@@ -61,10 +58,8 @@ var populateList = function () {
    $('#citySearched').empty();
    var cityHistory = JSON.parse(localStorage.getItem('cityHistory'));
    for (var i = 0; i < cityHistory.length; i++) {
-      console.log(cityHistory[i]);
       $('#citySearched').append(`<li class="cityItem">${cityHistory[i]}</li>`);
    }
-   console.log(cityHistory);
 }
 
 // the click button for searching the city's weather
