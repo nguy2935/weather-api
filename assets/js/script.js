ca439159f7ba543fb31bc69062e10e17d9987bb4
unknown <Sophia Nguyen>
2021-12-11 12:26:07 -0600

    added localstorage to store cities searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,9 @@ var getCityWeather = function(cityName) {
          "windspeed": data.wind.speed,
          "humidity": data.main.humidity,
          "lat": data.coord.lat,
-         "lon": data.coord.lon
-     }            
+         "lon": data.coord.lon,
+         "dt" : new Date(data.dt * 1000)
+     } 
 
      var urlUI = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${cityTemp.lat}&lon=${cityTemp.lon}&appid=${appID}`;
      $.getJSON( urlUI, function(data) {
@@ -25,7 +26,7 @@ var getCityWeather = function(cityName) {
      }).done(function(data) {
       cityTemp['uv'] = data.current.uvi;
       console.log(cityTemp);
-      $('.citySearch').html(cityTemp.name);
+      $('.citySearch').html(cityTemp.name + ` (${cityTemp.dt.toLocaleDateString("en-US")})`);
       $('.temp strong').html(cityTemp.temp + ' °F');
       $('.windspeed strong').html(cityTemp.windspeed + ' MPH');
       $('.humidity strong').html(cityTemp.humidity + '%');
@@ -49,11 +50,23 @@ var getCityWeather = function(cityName) {
     });
 }
 
+var populateList = function () {
+   var cityHistory = JSON.parse(localStorage.getItem('cityHistory'));
+   for (var i = 0; i < cityHistory.length; i++) {
+      console.log(cityHistory[i]);
+   }
+   console.log(cityHistory);
+}
+
 // the click button event listener
 var cities = [];
 $(document).on('click', '.searchBtn', function (event) {
    var cityName = $('.searchBar').val();
-   cities.push(cityName);
-   localStorage.setItem('cityHistory', cities);
+   // this checks to see if it duplicates stored in localstorage
+   if (!cities.includes(cityName)) {
+      cities.push(cityName);
+   }
+   localStorage.setItem('cityHistory', JSON.stringify(cities));
    getCityWeather(cityName);
+   populateList();
 });
